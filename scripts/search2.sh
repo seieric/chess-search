b530ed79e339ceca921143dde0fea0aa7383bcef
seieric <30688855+seieric@users.noreply.github.com>
2026-02-01 22:41:07 +0900

    add: scripts

---
+++ scripts/search2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# 駒がクイーンのとき4x5と5x6の盤面で先手必勝となる場所を探索する
+# 対称性があるため、左上の一部のみを調べる
+echo "[盤面1:4x5]"
+for row in 0 1; do
+  for col in 0 1 2; do
+    echo "初期位置: ($row, $col)"
+    python3 main.py 4 5 $row $col queen $@
+    echo ""
+  done
+done
+
+echo "[盤面2:5x6]"
+for row in 0 1 2; do
+  for col in 0 1 2; do
+    echo "初期位置: ($row, $col)"
+    python3 main.py 5 6 $row $col queen $@
+    echo ""
+  done
+done
---
+++ scripts/search3.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# 駒がナイトのとき7x7と8x8の盤面で先手必勝となる場所を探索する
+
+echo "[7x7盤面]"
+for row in 0 1 2 3; do
+  for col in `seq 0 $row`; do
+    echo "初期位置: ($row, $col)"
+    python3 main.py 7 7 $row $col knight $@
+    echo ""
+  done
+done
+
+echo "[8x8盤面]"
+for row in 0 1 2 3; do
+  for col in `seq 0 $row`; do
+    echo "初期位置: ($row, $col)"
+    python3 main.py 8 8 $row $col knight $@
+    echo ""
+  done
+done
