e4b7d580714abdc2489ff08b914121b5114680ad
seieric <30688855+seieric@users.noreply.github.com>
2026-02-02 01:55:44 +0900

    update: scripts

--- scripts/search1.sh
+++ scripts/search1.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
+: ${PROGRAM:=python3 python/main.py}
 # 初期状態1・ルーク
 echo "[初期状態1・ルーク]"
-time python3 main.py 3 3 2 2 rook $@
+time $PROGRAM 3 3 2 2 rook $@
 echo ""
 # 初期状態1・キング
 echo "[初期状態1・キング]"
-time python3 main.py 3 3 2 2 king $@
+time $PROGRAM 3 3 2 2 king $@
 echo ""
 # 初期状態2・ルーク
 echo "[初期状態2・ルーク]"
-time python3 main.py 4 4 3 3 rook $@
+time $PROGRAM 4 4 3 3 rook $@
 echo ""
 # 初期状態2・キング
 echo "[初期状態2・キング]"
-time python3 main.py 4 4 3 3 king $@
+time $PROGRAM 4 4 3 3 king $@
 echo ""
 # 初期状態3・ルーク
 echo "[初期状態3・ルーク]"
-time python3 main.py 5 4 3 3 rook $@
+time $PROGRAM 5 4 3 3 rook $@
 echo ""
 # 初期状態3・キング
 echo "[初期状態3・キング]"
-time python3 main.py 5 4 3 3 king $@
+time $PROGRAM 5 4 3 3 king $@
--- scripts/search3.sh
+++ scripts/search3.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+: ${PROGRAM:=python3 python/main.py}
 # 駒がナイトのとき7x7と8x8の盤面で先手必勝となる場所を探索する
 
 echo "[7x7盤面]"
 for row in 0 1 2 3; do
   for col in `seq 0 $row`; do
     echo "初期位置: ($row, $col)"
-    time python3 main.py 7 7 $row $col knight $@
+    time $PROGRAM 7 7 $row $col knight $@
     echo ""
   done
 done
@@ -14,7 +15,7 @@ echo "[8x8盤面]"
 for row in 0 1 2 3; do
   for col in `seq 0 $row`; do
     echo "初期位置: ($row, $col)"
-    time python3 main.py 8 8 $row $col knight $@
+    time $PROGRAM 8 8 $row $col knight $@
     echo ""
   done
 done
