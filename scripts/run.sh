607c9106395a26a970678e2c27e5a7a5f88f6d6e
seieric <30688855+seieric@users.noreply.github.com>
2026-02-03 00:20:14 +0900

    fix: run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,9 @@
 #!/bin.sh
-. python/.venv/bin/activate
+. .venv/bin/activate
+python3 --version
+echo "executing search1"
 ./scripts/search1.sh > logs/search1.log --heuristic --symmetry 2>&1
+echo "executing search2"
 ./scripts/search2.sh > logs/search2.log --heuristic --symmetry 2>&1
+echo "executing search3"
 ./scripts/search3.sh > logs/search3.log --heuristic --symmetry 2>&1
