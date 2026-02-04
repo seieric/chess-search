e2f2ed8023e1f6010414bad5e579c9de48af93b7
seieric <30688855+seieric@users.noreply.github.com>
2026-02-04 21:02:55 +0900

    rm: unused file

--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. .venv/bin/activate
-python3 --version
-echo "executing search1"
-./scripts/search1.sh > logs/search1.log --heuristic --symmetry 2>&1
-echo "executing search2"
-./scripts/search2.sh > logs/search2.log --heuristic --symmetry 2>&1
-echo "executing search3"
-./scripts/search3.sh > logs/search3.log --heuristic --symmetry 2>&1
